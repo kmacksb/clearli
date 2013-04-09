fbc4140e7acf03d4639f9ae98f8a73d9819f1811
Kyle Mack <kyle.mack531@gmail.com>
2013-04-09 15:52:55 -0400

    cloud cover

--- app.rb
+++ app.rb
@@ -22,16 +22,20 @@ get '/' do
 	@city = city
 	  
 	forecast = Forecast::IO.forecast(@lat, @long)
-	@current_temp = (((forecast.currently.temperature)-32)*(5.0/9.0)).ceil
+	@current_temp = (((forecast.currently.temperature)-32)*(5.0/9.0))
 	@current_wind = ((forecast.currently.windSpeed) * 1.609344).ceil
 	@current_icon = forecast.currently.icon
+	@current_clouds = forecast.currently.cloudCover
 
 	yesterday = Forecast::IO.forecast(@lat, @long, time: (Time.new.to_i - 86400))
 	@yesterday_temp = (((yesterday.currently.temperature)-32)*(5.0/9.0)).ceil
 	@yesterday_wind = ((yesterday.currently.windSpeed) * 1.609344).ceil
+	@yesterday_clouds = yesterday.currently.cloudCover
 
-	@temp_difference = (@current_temp - @yesterday_temp).ceil	
-	@wind_difference = (@current_wind - @yesterday_wind)
+	@temp_difference = (@current_temp - @yesterday_temp).ceil
+	@temp_difference_abs = @temp_difference.abs	
+	@wind_difference = ((@current_wind - @yesterday_wind)/@yesterday_wind)*100)
+	@cloud_difference = ((@current_clouds - @yesterday_clouds)/@yesterday_clouds)*100)
 
 	@temp_description = "warmer"
 		if @temp_difference < 0
@@ -53,9 +57,24 @@ get '/' do
 			@wind_description = "way less windy"
 		end
 
-	@unit = "degree"
-		unless @temp_difference == 1 || @temp_difference == -1
-			@unit = "degrees"
+	@cloud_description = "no brighter or cloudier than"
+		if (@cloud_difference >= 0 && @cloud_difference <= 25)
+			@cloud_description = "a bit brighter than"
+		elsif (@cloud_difference > 25 && @cloud_difference <= 75)
+			@cloud_description = "way brighter than"
+		elsif (@cloud_difference > 75)
+			@cloud_description = "a much more beautiful day than"
+		elsif (@cloud_difference < 0 && @cloud_difference >= -25)
+			@cloud_description = "a bit cloudier"
+		elsif (@cloud_difference < -25 && @cloud_difference >= -75)
+			@cloud_description = "way cloudier than"
+		elsif (@cloud_difference < -75)
+			@cloud_description = "not even as close to as nice as"	
+		end
+	
+	@unit = "degrees"
+		if @temp_difference == 1 || @temp_difference == -1
+			@unit = "degree"
 		end
 		
 		haml :index
