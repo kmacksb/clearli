5c69263e1e839648e194138c5e326fe7fa6f7718
Kyle Mack <kyle.mack531@gmail.com>
2013-04-09 17:17:50 -0400

    thans

--- app.rb
+++ app.rb
@@ -59,13 +59,13 @@ get '/' do
 
 	@cloud_description = "no brighter or cloudier than"
 		if (@cloud_difference >= 0 && @cloud_difference <= 25)
-			@cloud_description = "a bit cloudier"
+			@cloud_description = "a bit cloudier than"
 		elsif (@cloud_difference > 25 && @cloud_difference <= 75)
 			@cloud_description = "much more cloudy than"
 		elsif (@cloud_difference > 75)
 			@cloud_description = "disgustingly cloudy compared to"
 		elsif (@cloud_difference < 0 && @cloud_difference >= -25)
-			@cloud_description = "a bit sunnier"
+			@cloud_description = "a bit sunnier than"
 		elsif (@cloud_difference < -25 && @cloud_difference >= -75)
 			@cloud_description = "way brighter than"
 		elsif (@cloud_difference < -75)
